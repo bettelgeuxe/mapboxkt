007a9b2d723f12cbae31b71879f2ce9a82c9b6fa
Catherine Escobar Trujillo <bettelgeuxe@hotmail.com>
2022-09-22 15:32:13 -0500

    proyecto mapbox angular y nodjs

---
+++ front/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapPk: 'pk.eyJ1IjoiY2F0aGVyaW5lc2NvYmFydCIsImEiOiJjbDhjMnY4NjUwd2p2M3B0ZXN0ajg1aDN1In0.Y3QyO1hDzghAFK1odhzhNA'
+};
---
+++ front/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  mapPk: 'pk.eyJ1IjoiY2F0aGVyaW5lc2NvYmFydCIsImEiOiJjbDhjMnY4NjUwd2p2M3B0ZXN0ajg1aDN1In0.Y3QyO1hDzghAFK1odhzhNA'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
